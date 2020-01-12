4f6053aaea1867e701cedb03960a15109c25ccda
sevidmusic <sevidmusic@gmail.com>
2020-01-12 14:04:50 -0500

    Further progress with newComponent.php

--- DCMS/newComponent.sh
+++ DCMS/newComponent.sh
@@ -1,29 +1,39 @@
 #!/bin/sh
+
 notifyUser() {
-    notify-send "${1}";
-    printf "${1}\n\n";
+    MSG=$(printf "\n${1}\n\n");
+    printf "${1}";
+    notify-send "${MSG}";
 }
 
 promptUser() {
     notifyUser "${1}";
-    read -p 'Prompt: ' USER_INPUT;
+    PROMPT_MSG=$(printf "\nPrompt: ");
+    read -p "${PROMPT_MSG}" USER_INPUT;
 }
 
 promptUserAndVerifyInput() {
     while :
     do
+        clear;
+
         promptUser "${1}";
 
         notifyUser  "You entered \"${USER_INPUT}\"...is this correct";
 
-        promptUser "Enter \"Y\" to continue, \"N\" to change name...";
+        promptUser "Enter \"Y\" to continue, \"N\" to change...";
 
         if [ "${USER_INPUT}" = "Y" ]; then
+            clear;
             break;
         fi
-
     done;
 
+}
+
+generateTestTrait() {
+    TEST_TRAIT_CODE=$(sed "s/DS_COMPONENT_SUBTYPE/USER_DEFINED_COMPONENT_SUBTYPE/g; s/DS_COMPONENT_NAME/USER_DEFINED_COMPONENT_NAME/g" "/home/sevidmusic/Code/DarlingShells/DCMS/templates/Tests/Unit/interfaces/TestTraits/Component.php");
+    promptUserAndVerifyInput "The following code was generated for the Test Trait, please review it to make sure there are not any errors\n\n${TEST_TRAIT_CODE}\n\n"
 }
 
 notifyUser "Creating new Darling Cms Component";
@@ -34,11 +44,12 @@ do
     promptUserAndVerifyInput "Please enter a name for the component";
     # 2. Ask user for Component sub-type (used to determine the namespaces of
     #    the classes and test clases that define and test the component.
-    promptUserAndVerifyInput "Please enter the component's sub-type,\nThe sub-type is used to determine the appropriate namespaces for\nthe interface, implmentations, test trait, and test classes of the component.\nNote: You must escape backslash characters.\nHint: All components have a base namespace similar to \"\\DarlingCms\\FOO\\component\\...\"\n      The sub-type indicates the namespace under the base namespace.\ne.g., \\DarlingCms\\FOO\\component\\SUB\\TYPE\\COMPONENT_NAME\nDo not inlcude a preceding backslash in the sub-type, i.e., Wrong: \"\\\\Foo\\\\Bar\", Right: \"Foo\\\\Bar\"\nAgain, even though the examples dont excape their backslases, you MUST escape backslashes.";
+    promptUserAndVerifyInput "Please enter the component's sub-type, the sub-type\ndetermines the namespace pattern used to define the namespaces\nof the interface, implementations, test trait, and test classes\nrelated to the component.\n\nExample namespace pattern:\n\\DarlingCms\\\*\\component\\SUB\\TYPE\\COMPONENT_NAME\n\nNote: You must escape backslash characters.\n\nNote: Do not inlcude a preceding backslash in the sub-type.\nWrong: \\\\Foo\\\\Bar\nRight: Foo\\\\Bar\n";
     # 3. Generate Interface
     # 4. Generate Abstraction
     # 5. Generate Class
     # 6. Generate TestTrait
+    generateTestTrait;
     # 7. Generate Abstract Test
     # 8. Generate Test
     # 9. Optionally, run phpunit
@@ -47,5 +58,5 @@ do
 
 done;
 
-notify-send "Done...";
-printf "Done...";
+notifyUser "\n\n\n------- All done. Thank you for using the Darling Shell -------\n\n\n";
+
---
+++ DCMS/templates/Tests/Unit/interfaces/TestTraits/Component.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace UnitTests\interfaces\component\DS_COMPONENT_SUBTYPE\TestTraits;
+
+use DarlingCms\interfaces\component\DS_COMPONENT_SUBTYPE\DS_COMPONENT_NAME;
+
+trait DS_COMPONENT_NAMETestTrait
+{
+
+    private $DS_COMPONENT_NAME;
+
+    public function getDS_COMPONENT_NAME(): DS_COMPONENT_NAME
+    {
+        return $this->DS_COMPONENT_NAME;
+    }
+
+    public function setDS_COMPONENT_NAME(DS_COMPONENT_NAME $DS_COMPONENT_NAME)
+    {
+        $this->DS_COMPONENT_NAME = $DS_COMPONENT_NAME;
+    }
+
+}
+
+

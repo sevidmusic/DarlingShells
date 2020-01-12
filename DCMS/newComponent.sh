862c8d51767e0d184bc147577027438b060751a0
sevidmusic <sevidmusic@gmail.com>
2020-01-12 03:42:08 -0500

    Further progress on newcomponent.sh

--- DCMS/newComponent.sh
+++ DCMS/newComponent.sh
@@ -34,7 +34,7 @@ do
     promptUserAndVerifyInput "Please enter a name for the component";
     # 2. Ask user for Component sub-type (used to determine the namespaces of
     #    the classes and test clases that define and test the component.
-    promptUserAndVerifyInput "Please enter the component's sub type, used to determine approproate namespaces.";
+    promptUserAndVerifyInput "Please enter the component's sub-type,\nThe sub-type is used to determine the appropriate namespaces for\nthe interface, implmentations, test trait, and test classes of the component.\nNote: You must escape backslash characters.\nHint: All components have a base namespace similar to \"\\DarlingCms\\FOO\\component\\...\"\n      The sub-type indicates the namespace under the base namespace.\ne.g., \\DarlingCms\\FOO\\component\\SUB\\TYPE\\COMPONENT_NAME\nDo not inlcude a preceding backslash in the sub-type, i.e., Wrong: \"\\\\Foo\\\\Bar\", Right: \"Foo\\\\Bar\"\nAgain, even though the examples dont excape their backslases, you MUST escape backslashes.";
     # 3. Generate Interface
     # 4. Generate Abstraction
     # 5. Generate Class

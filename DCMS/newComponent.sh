355bbce97997f4184b94d418ef0cb1532ec4e4f6
sevidmusic <sevidmusic@gmail.com>
2020-01-12 14:52:12 -0500

    Further progress on newComponent.sh

--- DCMS/newComponent.sh
+++ DCMS/newComponent.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 notifyUser() {
-    MSG=$(printf "\n\n${1}\n\n");
-    printf "\n\n${1}\n\n";
+    MSG=$(printf "\n${1}\n");
+    printf "\n${1}\n";
     notify-send "${MSG}";
 }
 
 promptUser() {
     notifyUser "${1}";
     PROMPT_MSG=$(printf "\n\$dsh: ");
-    printf "\n(To use the Darling Shell: Type input if asked | Press <enter> to submit or continue)\n";
     read -p "${PROMPT_MSG}" USER_INPUT;
 }
 
@@ -20,9 +19,11 @@ promptUserAndVerifyInput() {
 
         promptUser "${1}";
 
+        clear;
+
         notifyUser  "You entered \"${USER_INPUT}\"\n\nIs this correct?";
 
-        promptUser "If everything looks ok, enter \"Y\" to continue,\npress any other key to repeat last step.";
+        promptUser "If so, type \"Y\" and press <enter> to continue,\npress <enter> repeat last step.";
 
         if [ "${USER_INPUT}" = "Y" ]; then
             clear;
@@ -37,15 +38,17 @@ generateTestTrait() {
     promptUserAndVerifyInput "The following code was generated for the Test Trait, please review it to make sure there are not any errors\n\n${TEST_TRAIT_CODE}\n\n"
 }
 
-notifyUser "Creating new Darling Cms Component";
 
 while :
 do
+    # 0. SHOW WELCOME MESSAGE
     # 1. Ask user for Component name
     promptUserAndVerifyInput "Please enter a name for the component";
+    USER_DEFINED_COMPONENT_NAME="${USER_INPUT}";
     # 2. Ask user for Component sub-type (used to determine the namespaces of
     #    the classes and test clases that define and test the component.
     promptUserAndVerifyInput "Please enter the component's sub-type, the sub-type\ndetermines the namespace pattern used to define the namespaces\nof the interface, implementations, test trait, and test classes\nrelated to the component.\n\nExample namespace pattern:\n\\DarlingCms\\\*\\component\\SUB\\TYPE\\COMPONENT_NAME\n\nNote: You must escape backslash characters.\n\nNote: Do not inlcude a preceding backslash in the sub-type.\nWrong: \\\\Foo\\\\Bar\nRight: Foo\\\\Bar\n";
+    USER_DEFINED_COMPONENT_SUBTYPE="${USER_INPUT}";
     # 3. Generate Interface
     # 4. Generate Abstraction
     # 5. Generate Class

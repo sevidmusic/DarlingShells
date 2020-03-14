126b57ee5f539baaff5b8a39cb13e114a5222d8c
sevidmusic <sevidmusic@gmail.com>
2020-03-14 18:21:56 -0400

    More refactoring of newComponent.sh

--- DCMS/newComponent.sh
+++ DCMS/newComponent.sh
@@ -10,14 +10,14 @@ askUserForSelection() {
   select opt in "${OPTIONS[@]}"; do
     case $opt in
     ${OPTIONS[$(("${REPLY}" - 1))]})
-      if [ -n "${opt}" ]; then
+      if [[ -n "${opt}" ]]; then
         RESPONSE_INDEX=$(("${REPLY}" - 1))
         RESPONSE=$(echo "${RESPONSES[${RESPONSE_INDEX}]}" | sed -E "s,-, ,g;")
         promptUserAndNotify "${RESPONSE}"
         PREVIOUS_USER_INPUT="${opt}"
         break
       fi
-      echo "${WARNINGCOLOR}${REPLY} is not a valid option, please enter the number that corresponds to your selction."
+      echo "${WARNINGCOLOR}${REPLY} is not a valid option, please enter the number that corresponds to your selection."
       ;;
     esac
   done
@@ -32,10 +32,10 @@ initVars() {
   INPUTCOLOR=$(setColor 34)
   PHPCODECOLOR=$(setColor 35)
   OPTIONS=("Core" "Extension")
-  RESPONSES=("${WARNINGCOLOR}WARNING:-Defing-new-Components-for-core-should-only-be-done-if-absolutely-necessary,-and-you-should-only-do-so-if-you-are-sure-you-know-what-you-are-doing-and-understand-the-consequences!\n\nIt-is-recomendd-that-you-define-new-Componets-as-part-of-an-Extension.\n\nIf-you-chose-to-proceed,-you-have-been-warned,-good-luck-with-your-new-component,-know-bad-things-may-happen,-and-if-they-do-it-will-probably-be-your-new-components-fault.-\n\nAre-you-sure-you-want-to-proceed?\n(Type-Y-and-press-<enter>-to-continue,-press-<ctrl>-c-to-quit-and-start-over." "You-have-chosen-to-create-a-new-Component-for-an-Extension,-if-this-is-not-correct-press-<ctrl>-c-to-quit,-otherwise-type-Y-and-press-<enter>\n")
+  RESPONSES=("${WARNINGCOLOR}WARNING:-Defining-new-Components-for-core-should-only-be-done-if-absolutely-necessary,-and-you-should-only-do-so-if-you-are-sure-you-know-what-you-are-doing-and-understand-the-consequences!-It-is-recommended-that-you-define-new-Components-as-part-of-an-Extension.-If-you-chose-to-proceed,-you-have-been-warned,-good-luck-with-your-new-component,-know-bad-things-may-happen,-and-if-they-do-it-will-probably-be-your-new-components-fault.-Are-you-sure-you-want-to-proceed?-(Type-Y-and-press-<enter>-to-continue,-press-<ctrl>-c-to-quit-and-start-over." "You-have-chosen-to-create-a-new-Component-for-an-Extension,-if-this-is-not-correct-press-<ctrl>-c-to-quit,-otherwise-type-Y-and-press-<enter>")
   askUserForSelection "${NOTIFYCOLOR}Is this Component being defined as part of Core or as part of an Extension?" $OPTIONS $RESPONSES
   COMPONENT_EXTENDS="${PREVIOUS_USER_INPUT}"
-  if [ "${COMPONENT_EXTENDS}" == "Core" ]; then
+  if [[ "${COMPONENT_EXTENDS}" == "Core" ]]; then
     EXTENSION_NAME=""
     COMPONENT_TEST_TRAIT_TARGET_ROOT_DIR="./Tests/Unit/interfaces/component"
     COMPONENT_ABSTRACT_TEST_TARGET_ROOT_DIR="./Tests/Unit/abstractions/component"
@@ -45,7 +45,7 @@ initVars() {
     COMPONENT_CLASS_TARGET_ROOT_DIR="./core/classes/component"
   fi
 
-  if [ "${PREVIOUS_USER_INPUT}" == "Extension" ]; then
+  if [[ "${PREVIOUS_USER_INPUT}" == "Extension" ]]; then
     promptUserAndVerifyInput "What is the name of the Extension this Component will belong to?"
     EXTENSION_NAME="${PREVIOUS_USER_INPUT}"
     COMPONENT_TEST_TRAIT_TARGET_ROOT_DIR="./Extensions/${EXTENSION_NAME}/Tests/Unit/interfaces/component"
@@ -81,9 +81,9 @@ showLoadingBar() {
   sleepWriteWordSleep "${1}" .3
   setColor 44
   INC=0
-  while [ $INC -le 42 ]; do
+  while [[ ${INC} -le 42 ]]; do
     sleepWriteWordSleep ":" .009
-    INC=$(($INC + 1))
+    INC=$((INC + 1))
   done
   echo "[100%]"
   setColor 0
@@ -108,13 +108,13 @@ promptUserAndVerifyInput() {
     clear
     promptUser "${1}"
     clear
-    notifyUser "You entered \"${INPUTCOLOR}${USER_INPUT}${NOTIFYCOLOR}\"\n\nIs this correct?"
-    if [ "${USER_INPUT}" = "Y" ]; then
+    notifyUser "You entered \"${INPUTCOLOR}${USER_INPUT}${NOTIFYCOLOR}\"Is this correct?"
+    if [[ "${USER_INPUT}" == "Y" ]]; then
       clear
       break
     fi
-    promptUser "If so, type ${INPUTCOLOR}\"Y\"${NOTIFYCOLOR} and press ${INPUTCOLOR}<enter>${NOTIFYCOLOR} to continue to next step,\nor just press ${INPUTCOLOR}<enter>${NOTIFYCOLOR} to repeat the last step."
-    if [ "${USER_INPUT}" = "Y" ]; then
+    promptUser "If so, type ${INPUTCOLOR}\"Y\"${NOTIFYCOLOR} and press ${INPUTCOLOR}<enter>${NOTIFYCOLOR} to continue to next step, or just press ${INPUTCOLOR}<enter>${NOTIFYCOLOR} to repeat the last step."
+    if [[ "${USER_INPUT}" == "Y" ]]; then
       clear
       break
     fi
@@ -126,7 +126,7 @@ promptUserAndNotify() {
     clear
     promptUser "${1}"
     clear
-    if [ "${USER_INPUT}" = "Y" ]; then
+    if [[ "${USER_INPUT}" == "Y" ]]; then
       showLoadingBar "${2}"
       clear
       break
@@ -139,14 +139,14 @@ generatePHPCodeFromTemplate() {
   GENERATED_FILE_ROOT_DIR_PATH="${2}"
   FILE_NAME_SUFFIX="${3}"
   GENERATED_FILE_PATH=$(echo "${GENERATED_FILE_ROOT_DIR_PATH}/${USER_DEFINED_COMPONENT_SUBTYPE}/${USER_DEFINED_COMPONENT_NAME}${FILE_NAME_SUFFIX}.php" | sed -E "s,\\\,/,g; s,//,/,g;")
-  if [ "${FILE_NAME_SUFFIX}" = "TestTrait" ]; then
+  if [[ "${FILE_NAME_SUFFIX}" == "TestTrait" ]]; then
     GENERATED_FILE_PATH=$(echo "${GENERATED_FILE_ROOT_DIR_PATH}/${USER_DEFINED_COMPONENT_SUBTYPE}/TestTraits/${USER_DEFINED_COMPONENT_NAME}${FILE_NAME_SUFFIX}.php" | sed -E "s,\\\,/,g; s,//,/,g;")
   fi
   PHP_CODE=$(sed -E "s/DS_EXTENSION_NAME/${EXTENSION_NAME}/g; s/DS_PARENT_COMPONENT_SUBTYPE/${USER_DEFINED_PARENT_COMPONENT_SUBTYPE}/g; s/DS_PARENT_COMPONENT_NAME/${USER_DEFINED_PARENT_COMPONENT_NAME}/g; s/DS_COMPONENT_SUBTYPE/${USER_DEFINED_COMPONENT_SUBTYPE}/g; s/DS_COMPONENT_NAME/${USER_DEFINED_COMPONENT_NAME}/g; s/[$][A-Z]/\L&/g; s/->[A-Z]/\L&/g; s/\\\\\\\/\\\/g; s/\\\;/;/g;" "${1}")
   GENERATED_FILE_SUB_DIR_PATH=$(echo "${GENERATED_FILE_PATH}" | sed -E "s/\/${USER_DEFINED_COMPONENT_NAME}${FILE_NAME_SUFFIX}.php//g")
-  printf "%sThe following code was generated using the %s%s%s template, please review it to make sure there are not any errors:%s\n\n" "${NOTIFYCOLOR}" "${INPUTCOLOR}" "${TEMPLATE}" "${NOTIFYCOLOR}" "${CLEARCOLOR}"
+  printf "\n\n%sThe following code was generated using the %s%s%s template, please review it to make sure there are not any errors:%s\n\n" "${NOTIFYCOLOR}" "${INPUTCOLOR}" "${TEMPLATE}" "${NOTIFYCOLOR}" "${CLEARCOLOR}"
   echo "${PHPCODECOLOR}${PHP_CODE}"
-  promptUser "\n\nIf everything looks ok press <enter>"
+  promptUser "If everything looks ok press <enter>"
   showLoadingBar "Writing file ${GENERATED_FILE_PATH} "
   mkdir -p "${GENERATED_FILE_SUB_DIR_PATH}"
   echo "${PHP_CODE}" >"${GENERATED_FILE_PATH}"
@@ -158,8 +158,9 @@ askUserForComponentName() {
 }
 
 askUserForComponentSubtype() {
-  promptUserAndVerifyInput "Please enter the component's sub-type, the sub-type\ndetermines the namespace pattern used to define the namespaces\nof the interface, implementations, test trait, and test classes\nrelated to the component.\n\nExample namespace pattern:\n\\DarlingCms\\\*\\component\\SUB\\TYPE\\${USER_DEFINED_COMPONENT_NAME}\n\nNote: You must escape backslash characters.\n\nNote: Do not inlcude a preceding backslash in the sub-type.\nWrong: \\\\Foo\\\\Bar\nRight: Foo\\\\Bar\n"
-  USER_DEFINED_COMPONENT_SUBTYPE=$(echo "${PREVIOUS_USER_INPUT}" | sed 's,\\,\\\\,g')
+  promptUserAndVerifyInput "Please enter the component's sub-type, the sub-type determines the namespace pattern used to define the namespaces of the interface, implementations, test trait, and test classes related to the component. Example namespace pattern: \\DarlingCms\\\*\\component\\SUB\\TYPE\\${USER_DEFINED_COMPONENT_NAME} Note: You must escape backslash characters. Note: Do not include a preceding backslash in the sub-type. Wrong: \\\\Foo\\\\Bar Right: Foo\\\\Bar"
+  #USER_DEFINED_COMPONENT_SUBTYPE=$(echo "${PREVIOUS_USER_INPUT}" | sed 's,\\,\\\\,g')
+  USER_DEFINED_COMPONENT_SUBTYPE=${PREVIOUS_USER_INPUT/\\/\\\\}
 }
 
 askUserForParentComponentName() {
@@ -169,7 +170,8 @@ askUserForParentComponentName() {
 
 askUserForParentComponentSubtype() {
   promptUserAndVerifyInput "Please enter the subtype of the component this component extends:"
-  USER_DEFINED_PARENT_COMPONENT_SUBTYPE=$(echo "${PREVIOUS_USER_INPUT}" | sed 's,\\,\\\\,g')
+  #USER_DEFINED_PARENT_COMPONENT_SUBTYPE=$(echo "${PREVIOUS_USER_INPUT}" | sed 's,\\,\\\\,g')
+  USER_DEFINED_PARENT_COMPONENT_SUBTYPE=${PREVIOUS_USER_INPUT/\\/\\\\}
 }
 
 showWelcomeMessage() {
@@ -248,21 +250,18 @@ askUserForTemplateDirectoryName() {
   ABSTRACTION_TEMPLATE_FILE_PATH="./templates/${TEMPLATE}/Abstraction.php"
   CLASS_TEMPLATE_FILE_PATH="./templates/${TEMPLATE}/Class.php"
 }
-while :; do
-  showWelcomeMessage
-  initVars
-  askUserForTemplateDirectoryName
-  askUserForParentComponentName
-  askUserForParentComponentSubtype
-  askUserForComponentName
-  askUserForComponentSubtype
-  generatePHPCodeFromTemplate "${TEST_TRAIT_TEMPLATE_FILE_PATH}" "${COMPONENT_TEST_TRAIT_TARGET_ROOT_DIR}" "TestTrait"
-  generatePHPCodeFromTemplate "${ABSTRACT_TEST_TEMPLATE_FILE_PATH}" "${COMPONENT_ABSTRACT_TEST_TARGET_ROOT_DIR}" "Test"
-  generatePHPCodeFromTemplate "${TEST_TEMPLATE_FILE_PATH}" "${COMPONENT_TEST_TARGET_ROOT_DIR}" "Test"
-  generatePHPCodeFromTemplate "${INTERFACE_TEMPLATE_FILE_PATH}" "${COMPONENT_INTERFACE_TARGET_ROOT_DIR}" ""
-  generatePHPCodeFromTemplate "${ABSTRACTION_TEMPLATE_FILE_PATH}" "${COMPONENT_ABSTRACTION_TARGET_ROOT_DIR}" ""
-  generatePHPCodeFromTemplate "${CLASS_TEMPLATE_FILE_PATH}" "${COMPONENT_CLASS_TARGET_ROOT_DIR}" ""
-  setColor 0
-  break
 
-done
+showWelcomeMessage
+initVars
+askUserForTemplateDirectoryName
+askUserForParentComponentName
+askUserForParentComponentSubtype
+askUserForComponentName
+askUserForComponentSubtype
+generatePHPCodeFromTemplate "${TEST_TRAIT_TEMPLATE_FILE_PATH}" "${COMPONENT_TEST_TRAIT_TARGET_ROOT_DIR}" "TestTrait"
+generatePHPCodeFromTemplate "${ABSTRACT_TEST_TEMPLATE_FILE_PATH}" "${COMPONENT_ABSTRACT_TEST_TARGET_ROOT_DIR}" "Test"
+generatePHPCodeFromTemplate "${TEST_TEMPLATE_FILE_PATH}" "${COMPONENT_TEST_TARGET_ROOT_DIR}" "Test"
+generatePHPCodeFromTemplate "${INTERFACE_TEMPLATE_FILE_PATH}" "${COMPONENT_INTERFACE_TARGET_ROOT_DIR}" ""
+generatePHPCodeFromTemplate "${ABSTRACTION_TEMPLATE_FILE_PATH}" "${COMPONENT_ABSTRACTION_TARGET_ROOT_DIR}" ""
+generatePHPCodeFromTemplate "${CLASS_TEMPLATE_FILE_PATH}" "${COMPONENT_CLASS_TARGET_ROOT_DIR}" ""
+setColor 0

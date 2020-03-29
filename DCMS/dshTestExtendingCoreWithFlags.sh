c0339065a8b03c79a1221cb9f8ca42bff4c61830
sevidmusic <sevidmusic@gmail.com>
2020-03-29 16:13:41 -0400

    gst

---
+++ DCMS/dshTestExtendingCoreWithFlags.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -x "Core" -c "Bazzer" -s "Bar\\Foo" -t "CoreComponent"
+/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -x "Core" -c "Foo" -s "Foo" -t "CoreOutputComponent"
+/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -x "Core" -c "Bar" -s "" -t "CoreSwitchableComponent"
+/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -x "Core" -c "Bar" -s "Foo\\Bar\\Bazzer" -t "CoreSwitchableComponent"
+
+

b52acca3364829eaacceeacc41e9281ba836c9aa
sevidmusic <sevidmusic@gmail.com>
2020-03-29 23:13:32 -0400

    Cleaned up and refactored newComponent.sh tests

--- DCMS/newComponent_TestForceMakeTests_RandomFlagOrder.sh
+++ DCMS/newComponent_TestForceMakeTests_RandomFlagOrder.sh
@@ -5,5 +5,5 @@
 /home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -t "CoreOutputComponent" -e "FooEx" -c "Foo" -s "" -x "Extension" -f &&
 /home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -e "FooEx" -c "Bazing" -s "BarBaz\\Bazzer" -x "Extension" -t "CoreComponent" -f &&
 /home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -c "Ifj" -s "a\\b\\c\\d" -x "Extension" -t "CoreSwitchableComponent" -e "FooEx" -f &&
-/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -s "a\\b\\c\\Baz" -x "Extension" -t "CoreComponent" -e "FooEx" -c "Iue" -f &&
+/home/sevidmusic/Code/DarlingShells/DCMS/newComponent.sh -s "a\\b\\c\\Baz" -x "Extension" -t "CoreComponent" -e "FooEx" -c "Iue" -f
 

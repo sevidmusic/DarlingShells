d0065bf0477152acc544e787e3f95ec95fc31c67
sevidmusic <sevidmusic@gmail.com>
2020-04-10 08:04:35 -0400

    Refactored print.sh to use function to animate script

--- print.sh
+++ print.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 read input
+
 text=$( echo "${input}" | sed -E "s/ /_/g;")
-for (( i=0; i< ${#text}; i++ )); do
-    printf "%s" ${text:$i:1}
-    sleep $1
-done
+speed=$1
+
+animatedPrint()
+{
+  local _text, _speed
+  _text="${1}"
+  _speed="${2}"
+  printf "\n\nText:%s\n\nSpeed:%s\n\n" "${_text}" $_speed
+  for (( i=0; i< ${#_text}; i++ )); do
+      printf "%s" ${_text:$i:1}
+      sleep $_speed
+  done
+}
+
+animatedPrint "${text}" $speed

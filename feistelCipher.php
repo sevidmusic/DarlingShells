1ac4d0f8ebb04f11b9e808db9f4dc7f5a542ef7f
sevidmusic <sevidmusic@gmail.com>
2020-04-21 03:12:53 -0400

    Various changes

--- feistelCipher.php
+++ feistelCipher.php
@@ -1,77 +1,59 @@
 <?php
-// Rough overview
-// get byte array
-//
-// split array in two | i.e. Left and Right
-//
-// Swap Left and Right
-//
-// XOR
-//
-// Repeat as many times as you wish
-//
 
-
-$plainText = 'Hello Dad';
-
-// WARNING: unpack() returns an array with of-1 based indexes, i.e. indexes start at 1, not 0
-// list() is used below, and expects array indexes to start at 0, so $byteArray indexes will have to be adjusted
-$byteArray = unpack('C*', $plainText);
-
-$byteArrayCorrectedIndexes = [];
-foreach($byteArray as $byte) {
-    array_push($byteArrayCorrectedIndexes, $byte);
-}
-
-list($left, $right) = array_chunk($byteArrayCorrectedIndexes, ceil(count($byteArrayCorrectedIndexes) / 2));
-
-
-
-/// dev
-// fiestel works as follows
-// plainText / 2 =  Left | Right
-// Right xor Left
-// Repeat
-$limitString = '(EBYTE - BITS)';
-$keyFormat = '(EBYTE = BITS)%s=    (RIGHTBYTE = BITS)    ^    (LEFTBYTE = BITS)' . PHP_EOL;
-$byteFormat = '(%1$2d = %1$04b)%2$s=    (%3$2d = %3$04b)    %4$s    (%5$2d = %5$04b)' . PHP_EOL . PHP_EOL;
-
-foreach($right as $index => $rightByte) {
-    $xor = $rightByte ^ $left[$index];
+/*
+foreach($byteArray as $lrBytes) {
+    $xor = $lrBytes['right'] ^ $lrBytes['left'][$index];
     $limitString = sprintf('(%1$2d = %1$04b)', $xor);
     printf($keyFormat, '    ');
-    printf($byteFormat,$xor, getSpaceing($limitString), $rightByte, '^', $left[$index]);
+    printf($byteFormat,$xor, getSpaceing($limitString), $lrBytes['right'], '^', $lrBytes['left'][$index]);
 }
-
-function getSpaceing(string $xor): string
+*/
+function getSpaceing(string $string): string
 {
     $spaces = '';
-    $spaceCount = (4 + (14 - strlen($xor))); // ( strlen($xor) < 14 ?  (14 - strlen($xor)) : 0 ));
-    /*var_dump(
-        [
-            'xor' => $xor,
-            'logic' =>  '(4 + ( ' . strlen($xor) . ' < 14 ?  (14 - ' . strlen($xor) . ') : 0 ))',
-            'SpaceCount' => $spaceCount
-        ]
-    );*/
+    $spaceCount = (4 + (14 - strlen($string)));
     for($i=0; $i<$spaceCount;$i++) {
         $spaces .= ' ';
     }
     return $spaces;
 }
 
+function convertToByteArray(string $plainText): array
+{
+    // WARNING: unpack() returns an array with of-1 based indexes, i.e. indexes start at 1, not 0
+    // list() is used below, and expects array indexes to start at 0, so $byteArray indexes will have to be adjusted
+    $byteArray = unpack('C*', $plainText);
+    $byteArrayCorrectedIndexes = [];
+    foreach($byteArray as $byte) {
+        array_push($byteArrayCorrectedIndexes, $byte);
+    }
+    list($left, $right) = array_chunk($byteArrayCorrectedIndexes, ceil(count($byteArrayCorrectedIndexes) / 2));
+    $lrBytes = ['left' => $left, 'right' => $right];
+    return $lrBytes;
+}
 
+$plainText = file_get_contents(__DIR__ . '/plainText.txt');
+$byteArray = convertToByteArray($plainText);
+$limitString = '(EBYTE - BITS)';
+$keyFormat = '(EBYTE = BITS)%s=    (RIGHTBYTE = BITS)    ^    (LEFTBYTE = BITS)' . PHP_EOL;
+$byteFormat = '(%1$2d = %1$04b)%2$s=    (%3$2d = %3$04b)    %4$s    (%5$2d = %5$04b)' . PHP_EOL . PHP_EOL;
+$difference = (count($byteArray['left']) - count($byteArray['right']));
+$randChar = $difference ^ $difference;
 
-// dev
-/*
-var_dump(
-    [
-        'plainText' => $plainText,
-        'bytesArray' =>$byteArray,
-        'correctedBytesArray' => $byteArrayCorrectedIndexes,
-        'left' => $left,
-        'right' => $right
-    ]
-);
- */
+foreach($byteArray['left'] as $index => $leftByte)
+{
+        $devOutput = [
+            'leftByte' => $leftByte,
+            'leftChar' => chr($leftByte),
+            'rightByte' => (isset($byteArray['right'][$index]) === true ? $byteArray['right'][$index] : $randChar),
+            'rightChar' => (isset($byteArray['right'][$index]) === true ? chr($byteArray['right'][$index]) : chr($randChar)),
+        ];
+        foreach($devOutput as $devKey => $devValue)
+        {
+            $color1 = sprintf("\e[%sm \e[%sm ", "34", "45");
+            $color2 = sprintf("\e[%sm", "35");
+            $resetColor = sprintf("\e[%sm", "0");
+            printf('%s%s%s%s%s : %s%s%s%s', PHP_EOL, $resetColor, $color1, $devKey, $resetColor, $color2, $devValue, $resetColor, PHP_EOL);
+        }
+}
 
--- loopPywal.sh
+++ loopPywal.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
+clear
 
 # source ~/.bash_functions || printf "Error: .bash_functions file not found"
 source ~/.bash_aliases || printf "Error: .bash_aliases file not found"
 
-[[ -z "${1}" ]] && printf "\nYou must speicfy the number of seconds that should pass before each pywal reload, i.e., number of seconds per loop cycle, note each loop may take a bit longer than number of seconds specified.\n" && exit
+[[ -z "${1}" ]] && setColor 36 && animatedPrint "Error: For loop to run you must speicfy the number of seconds that should pass before each pywal reload." .03 && printf "\n\n" && animatedPrint "i.e., The number of seconds per loop cycle." .03 && printf "\n\n" && animatedPrint "NOTE: The opening script takes takes about 9 seconds to run," .03 && printf "\n\n" && animatedPrint "      so add about 9 seconds to the number you specify and" .03 && printf "\n\n" && animatedPrint "      that will be the total time each loop will actually take to run." .03 && printf "\n\n" && setColor 0 && exit 0
 
 while :
 do
     showLoadingBar "Reloading Color Scheme"
     wal -q -i /home/sevidmusic/Wallpapers
     ~/gitClones/intellijPywal/intellijPywalGen.sh ~/.PhpStorm2019.3/config
+    neofetch --off --disable cpu gpu memory icons theme shell resolution packages wm de term uptime kernel
     showLoadingBar "Reload complete. Next reload in ${1} seconds"
-    neofetch
     sleep $1
 done
 

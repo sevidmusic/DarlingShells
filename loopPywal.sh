7246d7bd07e42723a21b936c6854ec5a51382601
sevidmusic <sevidmusic@gmail.com>
2020-06-08 14:28:11 -0400

    Various changes

--- loopPywal.sh
+++ loopPywal.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-clear
+#clear
 
-# source ~/.bash_functions || printf "Error: .bash_functions file not found"
-source ~/.bash_aliases || printf "Error: .bash_aliases file not found"
+# source ~/.bash_aliases || printf "Error: .bash_aliases file not found"
 
-[[ -z "${1}" ]] && setColor 36 && animatedPrint "Error: For loop to run you must speicfy the number of seconds that should pass before each pywal reload." .03 && printf "\n\n" && animatedPrint "i.e., The number of seconds per loop cycle." .03 && printf "\n\n" && animatedPrint "NOTE: The opening script takes takes about 9 seconds to run," .03 && printf "\n\n" && animatedPrint "      so add about 9 seconds to the number you specify and" .03 && printf "\n\n" && animatedPrint "      that will be the total time each loop will actually take to run." .03 && printf "\n\n" && setColor 0 && exit 0
+#[[ -z "${1}" ]] && setColor 36 && animatedPrint "Error: For loop to run you must speicfy the number of seconds that should pass before each pywal reload." .03 && printf "\n\n" && animatedPrint "i.e., The number of seconds per loop cycle." .03 && printf "\n\n" && animatedPrint "NOTE: The opening script takes takes about 9 seconds to run," .03 && printf "\n\n" && animatedPrint "      so add about 9 seconds to the number you specify and" .03 && printf "\n\n" && animatedPrint "      that will be the total time each loop will actually take to run." .03 && printf "\n\n" && setColor 0 && exit 0
 
 while :
 do

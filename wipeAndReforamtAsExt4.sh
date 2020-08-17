60491c9aa80ca5b6cc5ff186ab33be519ab90b10
sevidmusic <sevidmusic@gmail.com>
2020-08-17 07:36:23 -0400

    Continued development of wipeAndReformatAsExt4.sh

--- wipeAndReforamtAsExt4.sh
+++ wipeAndReforamtAsExt4.sh
@@ -58,13 +58,51 @@ showLoadingBar() {
   setColor 0
 }
 
+notifyUser() {
+  [[ "${2}" == "showInfo" ]] && showInfoPanel
+  printf "\n%s%s%s\n" "${NOTIFYCOLOR}" "${1}" "${CLEARCOLOR}"
+}
+
+promptUser() {
+  local _pu_promptMessage
+  notifyUser "${1}" "${2}"
+  _pu_promptMessage=$(printf "%s\n%s\$dsh: %s" "${CLEARCOLOR}" "${DSHCOLOR}${DARKTEXTCOLOR}" "${USRPRMPTCOLOR}")
+  PREVIOUS_USER_INPUT="${CURRENT_USER_INPUT}"
+  read -p "${_pu_promptMessage}" CURRENT_USER_INPUT
+  setColor 0
+}
+
+promptUserAndVerifyInput() {
+  while :; do
+    promptUser "${1}" "${2}"
+    notifyUser "You entered \"${CLEARCOLOR}${HIGHLIGHTCOLOR}${DARKTEXTCOLOR}${CURRENT_USER_INPUT}${CLEARCOLOR}${NOTIFYCOLOR}\"Is this correct?${CLEARCOLOR}" ""
+    promptUser "If so, type ${CLEARCOLOR}${HIGHLIGHTCOLOR}${DARKTEXTCOLOR}\"Y\"${CLEARCOLOR}${NOTIFYCOLOR} and press ${CLEARCOLOR}${HIGHLIGHTCOLOR}${DARKTEXTCOLOR}<enter>${CLEARCOLOR}${NOTIFYCOLOR} to continue to next step, or just press ${CLEARCOLOR}${HIGHLIGHTCOLOR}${DARKTEXTCOLOR}<enter>${CLEARCOLOR}${NOTIFYCOLOR} to repeat the last step.${CLEARCOLOR}"
+    if [[ "${CURRENT_USER_INPUT}" == "Y" ]]; then
+      showLoadingBar "Thank you, one moment please"
+      break
+    fi
+  done
+}
+
+promptUserAndNotify() {
+  setColor 0
+  while :; do
+    promptUser "${1}"
+    if [[ "${CURRENT_USER_INPUT}" == "Y" ]]; then
+      showLoadingBar "${2}"
+      break
+    fi
+  done
+}
+
+
 clear
 
 initColors
 
 [[ -z "$(which parted)" ]] && insertLines && animatedPrint "${CLEARCOLOR}${ATTENTIONEFFECT}${ATTENTIONEFFECTCOLOR}GNU Parted is required for this script to work.${CLEARCOLOR}" && insertLine && animatedPrint "Please install parted via your distribution's package manager" && insertLine && animatedPrint "Example on Debian based:" && insertLine && animatedPrint "sudo apt install parted" && insertLine && exit 1
 
-while getopts "hs" OPTION; do
+while getopts "hsd:" OPTION; do
   case "${OPTION}" in
   h)
       insertLines
@@ -85,7 +123,17 @@ while getopts "hs" OPTION; do
       insertLines
     exit
     ;;
-
+  d)
+      insertLines
+      animatedPrint "${CLEARCOLOR}${ATTENTIONEFFECT}${WARNINGCOLOR}WARNING${CLEARCOLOR}"
+      insertLines
+      animatedPrint "${CLEARCOLOR}${NOTIFYCOLOR}You are about to erase and reformat the drive named ${CLEARCOLOR}${WARNINGCOLOR}${OPTARG}${CLEARCOLOR}${NOTIFYCOLOR}"
+      insertLines
+      promptUser "Enter Y to continue, any other key to quit."
+      if [[ "${CURRENT_USER_INPUT}" == "Y" ]] || [[ $FORCE_MAKE -eq 1 ]]; then
+          showLoadingBar
+      fi
+      ;;
   *)
     printf "\nInvalid flag or invalid use of flag: %s\n\nFor help, use -h\n\n" "${OPTION}"
     exit

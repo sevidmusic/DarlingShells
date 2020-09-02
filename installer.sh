ff4b34508a9c4e0d3f5252f70045b54931c66818
sevidmusic <sdmwebsdm@gmail.com>
2020-09-02 18:11:40 +0000

    Continuned development of installer.sh

--- installer.sh
+++ installer.sh
@@ -10,7 +10,7 @@ initColors() {
   WARNINGCOLOR=$(setColor 35)
   CLEARCOLOR=$(setColor 0)
   NOTIFYCOLOR=$(setColor 33)
-  DSHCOLOR=$(setColor 41)
+  HIGHLIGHTCOLOR3=$(setColor 41)
   USRPRMPTCOLOR=$(setColor 41)
   HIGHLIGHTCOLOR=$(setColor 41)
   HIGHLIGHTCOLOR2=$(setColor 45)
@@ -22,32 +22,29 @@ initColors() {
 initMessages() {
     NEWLINE="\n\n"
     SCRIPTNAME=`basename "$(realpath $0)"`
-    BANNER="
-${CLEARCOLOR}${HIGHLIGHTCOLOR}     _                          ___
-${CLEARCOLOR}${NOTIFYCOLOR}    | \ _.._|o._  _   /\ .__|_   | ._  __|_ _.|| _ ._
-${CLEARCOLOR}${NOTIFYCOLOR}    |_/(_|| ||| |(_| /--\|(_| | _|_| |_> |_(_|||(/_|
-${CLEARCOLOR}${HIGHLIGHTCOLOR}                  _|
-${CLEARCOLOR}
-"
-    HELPMSG1="${CLEARCOLOR}${NOTIFYCOLOR}I developed ${CLEARCOLOR}${HIGHLIGHTCOLOR}${SCRIPTNAME}${CLEARCOLOR}${NOTIFYCOLOR} as a guide for myself."
-    HELPMSG2="It walks me through the process of installing Arch linux on a legacy BIOS using ext4 for a filesystem."
-    HELPMSG3="Passing the -p flag with a filename like:"
-    HELPMSG4="${CLEARCOLOR}${HIGHLIGHTCOLOR2}${SCRIPTNAME} -p /path/to/file${CLEARCOLOR}${NOTIFYCOLOR}"
-    HELPMSG5="will tell the installer to include the packages in the specified file in the final insallation."
-    HELPMSG6="Feel free to modify the script to suit your needs.${CLEARCOLOR}"
-    HELPMSG7="-Sevi D"
+    BANNER='
+   ___           ___             ___           __
+  / _ \___ _____/ (_)__  ___ _  / _ | ________/ /
+ / // / _ \/ __/ / / _ \/ _  / / __ |/ __/ __/ _ \
+/____/\_._/_/ /_/_/_//_/\_. / /_/ |_/_/  \__/_//_/
+                       /___/
+'
+    HELPMSG_OPENING1="I developed ${CLEARCOLOR}${HIGHLIGHTCOLOR}${SCRIPTNAME}${CLEARCOLOR}${NOTIFYCOLOR} as a guide for myself."
+    HELPMSG_OPENING2="It walks me through the process of installing Arch linux on a legacy BIOS using ext4 for a filesystem."
+    HELPMSG_CLOSING1="Feel free to modify the script to suit your needs."
+    HELPMSG_CLOSING2="-Sevi D"
     LB_PRE_INSTALL_MSG='Pre-installation will begin in a moment'
     LB_INSTALL_MSG='Insallation of Arch Linx will begin in a moment'
     LB_POST_INSTALL_MSG='Post-installation will being in a moment'
-    PWD_ISSET="${CLEARCOLOR}${NOTIFYCOLOR}Root password was already set, to reset run: ${CLEARCOLOR}${HIGHLIGHTCOLOR2}passwd${CLEARCOLOR}"
-    PLS_SET_PWD="${CLEARCOLOR}${NOTIFYCOLOR}Please set the root password:${CLEARCOLOR}"
-    PWD_ERROR_OCCURED="${CLEARCOLOR}${WARNINGCOLOR}An error may have occured, you may need to call passwd manually to set the root password${CLEARCOLOR}"
-    PWD_WAS_SET_FOR_ISO_WONT_PERSIST="${CLEARCOLOR}${NOTIFYCOLOR}The password you just set will ${CLEARCOLOR}${WARNINGCOLOR}NOT${CLEARCOLOR}${NOTIFYCOLOR} persist to the actual installation.${CLEARCOLOR}"
-    PWD_WAS_SET_USE_FOR_SSH_LOGIN="${CLEARCOLOR}${HIGHLIGHTCOLOR2}If the -s flag was supplied, then the password you just set can be used to login to the installation media as root via ssh.${CLEARCOLOR}"
-    IPINFOMSG1="${CLEARCOLOR}${NOTIFYCOLOR}The following is your ip info (obtained via ${CLEARCOLOR}${HIGHLIGHTCOLOR}ip a${CLEARCOLOR}${NOTIFYCOLOR}):${CLEARCOLOR}"
+    PWD_ISSET="Root password was already set, to reset run: ${CLEARCOLOR}${HIGHLIGHTCOLOR2}passwd${CLEARCOLOR}"
+    PLS_SET_PWD="Please set the root password:"
+    PWD_ERROR_OCCURED="${CLEARCOLOR}${WARNINGCOLOR}An error may have occured, you may need to manually set the root password for the installation media by running: ${CLEARCOLOR}${HIGHLIGHTCOLOR3}passwd${CLEARCOLOR}"
+    PWD_WAS_SET_FOR_ISO_WONT_PERSIST="${CLEARCOLOR}${WARNINGCOLOR}The password you just set will NOT persist onto the actual installation.${CLEARCOLOR}"
+    PWD_WAS_SET_USE_FOR_SSH_LOGIN="If the -s flag was supplied, then the password you just set will be the password you use to login to the installation media as root via ssh."
+    IPINFOMSG1="The following is your ip info (obtained via ${CLEARCOLOR}${HIGHLIGHTCOLOR}ip a${CLEARCOLOR}${NOTIFYCOLOR}):"
     STARTING_SSH_MSG="Attempting to start sshd"
     POST_SSH_INSTALL_EXIT_MSG="Exiting installer, re-run WTIHOUT -s flag to continue with installation"
-    SSH_LOGIN_AVAILABLE="${CLEARCOLOR}${NOTIFYCOLOR}You can now log into the installation media as root via ssh.${CLEARCOLOR}"
+    SSH_LOGIN_AVAILABLE="You can now log into the installation media as root via ssh."
 }
 
 animatedPrint()
@@ -90,11 +87,11 @@ showLoadingBar() {
 
 notifyUser()
 {
-    printf "\n"
+    printf "\n${CLEARCOLOR}${NOTIFYCOLOR}"
     animatedPrint "${1}" 0.009
     sleep ${2:-2}
     [[ "${3}" == "dontClear" ]] || clear
-    printf "\n"
+    printf "${CLEARCOLOR}\n"
 }
 
 setRootPassword()
@@ -116,8 +113,8 @@ showIpInfoMsg() {
 
 showPostSSHInstallMsg() {
     notifyUser "${SSH_LOGIN_AVAILABLE}" 1 'dontClear'
-    notifyUser "${CLEARCOLOR}${NOTIFYCOLOR}The installer will now exit to give you an oppurtunity to login via ssh.${CLEARCOLOR}" 1 'dontClear'
-    notifyUser "${CLEARCOLOR}${NOTIFYCOLOR}Whether or not you decide to login via ssh, to continue the installation process re-run ${SCRIPTNAME} without the -s flag:${CLEARCOLOR}" 1 'dontClear'
+    notifyUser "The installer will now exit to give you an oppurtunity to login via ssh." 1 'dontClear'
+    notifyUser "Whether or not you decide to login via ssh, to continue the installation process re-run ${CLEARCOLOR}${HIGHLIGHTCOLOR3}${SCRIPTNAME}${CLEARCOLOR}${NOTIFYCOLOR} without the -s flag:" 1 'dontClear'
     notifyUser "Example:" 1 'dontClear'
     notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}${SCRIPTNAME}${CLEARCOLOR}" 1 'dontClear'
     notifyUser "or" 1 'dontClear'
@@ -126,7 +123,7 @@ showPostSSHInstallMsg() {
 }
 
 showSSHLocationService() {
-    notifyUser "${CLEARCOLOR}${NOTIFYCOLOR}${SSH} location and sshd service info:${CLEARCOLOR}" 1 'dontClear'
+    notifyUser "${SSH} location and sshd service info:" 1 'dontClear'
     notifyUser "Location: ${CLEARCOLOR}${HIGHLIGHTCOLOR}$(which ssh)${CLEARCOLOR}" 1 'dontClear'
     notifyUser "Service:  ${CLEARCOLOR}${HIGHLIGHTCOLOR}$(systemctl list-units --type=service | grep ssh | awk '{ print $1 }')${CLEARCOLOR}" 3 'dontClear'
 }
@@ -143,13 +140,13 @@ showStartSSHExitMsg()
 startSSH()
 {
     if [[ "$(systemctl list-units --type=service | grep ssh | wc -l)" -gt 0 ]]; then
-        notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}${SSH}${CLEARCOLOR}${NOTIFYCOLOR} is already running: ${CLEARCOLOR}" 1 'dontClear'
+        notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}${SSH}${CLEARCOLOR}${NOTIFYCOLOR} is already running:" 1 'dontClear'
         showStartSSHExitMsg
     fi
     showLoadingBar "${STARTING_SSH_MSG}"
     systemctl start sshd
     [[ "$(systemctl list-units --type=service | grep ssh | wc -l)" -lt 1 ]] && printf "${NEWLINE}" && animatedPrint "Failed to start sshd. You may need to install/re-install/configure ${SSH}." && exit 1
-    notifyUser "${CLEARCOLOR}${NOTIFYCOLOR}SSH is now running, you should now be able to login via ssh${CLEARCOLOR}"
+    notifyUser "SSH is now running, you should now be able to login via ssh"
     showStartSSHExitMsg
 }
 
@@ -158,7 +155,7 @@ installWhich() {
     [[ -f ~/.cache/.installer_which ]] && printf "${NEWLINE}" && notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}which${CLEARCOLOR}${NOTIFYCOLOR} is already installed: ${CLEARCOLOR}${HIGHLIGHTCOLOR}$(which which)${CLEARCOLOR}" && return
     showLoadingBar "Installing \"which\" so program locations can be determined"
     pacman -S which --noconfirm
-    notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}which${CLEARCOLOR}${NOTIFYCOLOR} is now installed on the installation media, this ${CLEARCOLOR}${WARNINGCOLOR}will NOT persist${CLEARCOLOR}${NOTIFYCOLOR} onto the actual installation.${CLEARCOLOR}"
+    notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR}which${CLEARCOLOR}${NOTIFYCOLOR} is now installed on the installation media, this ${CLEARCOLOR}${WARNINGCOLOR}will NOT persist${CLEARCOLOR}${NOTIFYCOLOR} onto the actual installation."
     printf "which_already_installed" >> ~/.cache/.installer_which
 }
 
@@ -183,6 +180,16 @@ performPostInstallation() {
     showLoadingBar "${LB_POST_INSTALL_MSG}"
 }
 
+showHelpMsg()
+{
+      notifyUser "${HELPMSG_OPENING1}" 1 'dontClear'
+      notifyUser "${HELPMSG_OPENING2}" 1 'dontClear'
+      notifyUser "The -p flag can be used to specify a package file:" 1 'dontClear'
+      notifyUser "${CLEARCOLOR}${HIGHLIGHTCOLOR3}${SCRIPTNAME} -p /path/to/file${CLEARCOLOR}" 1 'dontClear'
+      notifyUser "Any packages named in the specified file will be included in the final insallation." 1 'dontClear'
+      notifyUser "${HELPMSG_CLOSING1}" 1 'dontClear'
+      notifyUser "${HELPMSG_CLOSING2}" 1 'dontClear'
+}
 ########################## PROGRAM #######################
 
 clear
@@ -194,13 +201,7 @@ while getopts ":hs" OPTION; do
   case "${OPTION}" in
   h)
       printf "%s" "${BANNER}"
-      notifyUser "${HELPMSG1}" 1 'dontClear'
-      notifyUser "${HELPMSG2}" 1 'dontClear'
-      notifyUser "${HELPMSG3}" 1 'dontClear'
-      notifyUser "${HELPMSG4}" 1 'dontClear'
-      notifyUser "${HELPMSG5}" 1 'dontClear'
-      notifyUser "${HELPMSG6}" 1 'dontClear'
-      notifyUser "${HELPMSG7}" 1 'dontClear'
+          showHelpMsg
       exit 1
     ;;
   s)
@@ -216,5 +217,7 @@ performPreInsallation
 # NOTE: Use a file to determine which packages are installed in addition to base. i.e. package.list
 performInstallation
 performPostInstallation
+
+
 
 

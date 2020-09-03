8f1b2f9291e28ec1b143b78b84525454d212f499
sevidmusic <sdmwebsdm&gmail.com>
2020-09-03 19:24:28 +0000

    Minor changes to setup.sh, everything is working

--- setup.sh
+++ setup.sh
@@ -9,11 +9,16 @@ error_dshells_repo_missing() {
 
 [[ -f ~/DarlingShells/darlingarch.sh ]] || error_dshells_repo_missing
 
+printf "\n\nInstalling vim, tmux, and htop\n\n" && sleep 2
 pacman -Syy tmux vim htop --noconfirm
 
+printf "\n\nConfiguring git\n\n" && sleep 2
 git config --global user.name "sevidmusic"
 git config --global user.email "sdmwebsdm&gmail.com"
 
+mkdir -p /root/.config/htop
+
+printf "\n\nsyncing user configuration files\n\n" && sleep 2
 rsync -c ~/DarlingShells/.autorsync ~/.autorsync
 rsync -c ~/DarlingShells/.bashrc ~/.bashrc
 rsync -c ~/DarlingShells/.bash_aliases ~/.bash_aliases
@@ -23,9 +28,17 @@ rsync -c ~/DarlingShells/.vimrc ~/.vimrc
 rsync -c ~/DarlingShells/darlingarch.sh ~/darlingarch.sh
 rsync -c ~/DarlingShells/htoprc /root/.config/htop/htoprc
 
-mkdir ~/Code
 
+printf "\n\nMaking ~/Code direcotry\n\n" && sleep 2
+[[ -d ~/Code ]] || mkdir ~/Code
+
+printf "\n\nMoving ~/DarlingShells to ~/Code/DarlingShells\n\n" && sleep 2
 mv ~/DarlingShells ~/Code/DarlingShells
 
-source ~/.bash_profile
+[[ -f ~/.bash_profile ]] && .~/.bash_profile
 
+printf "\n\nDone\n\n"
+
+pwd
+
+ls -a
